c1081d82be53a17b76ef057e0d5120fedd1ff0f5
alibaghn <alibaghn@gmail.com>
2022-12-09 20:37:03 -0800

    App skeleton

--- PingMe/ContentView.swift
+++ PingMe/ContentView.swift
@@ -5,15 +5,38 @@
 //  Created by Ali Bagherinia on 12/8/22.
 //
 
+import Firebase
 import SwiftUI
 
 struct ContentView: View {
+    let db = Firestore.firestore()
+
+    @State var textFieldValue: String = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("Placeholder", text: $textFieldValue)
+            Button("Send") {
+                var ref: DocumentReference?
+                ref = db.collection("users").addDocument(data: ["message": "\(textFieldValue)"]) {
+                    err in
+                    if let err = err {
+                        print("Error occured adding doc: \(err)")
+                    } else {
+                        print("Doc added with ID:\(ref!.documentID)")
+                        db.collection("users").getDocuments { snapShot, error in
+                            if let error {
+                                print("Error occured: \(error)")
+                            } else {
+                                guard let snapShot else { return }
+                                let docData = snapShot.documents.map { doc in
+                                    doc.data()
+                                }
+                                print(docData)
+                            }
+                        }
+                    }
+                }
+            }
         }
         .padding()
     }
